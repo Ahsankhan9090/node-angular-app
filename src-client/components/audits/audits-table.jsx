ffb60c1df64d7797a0331b1b20b978dd1b4d9b60
Your Coworker <your.coworker@woventeams.com>
2022-06-07 15:41:56 -0400

    Add pagination

--- src-client/components/audits/audits-table.jsx
+++ src-client/components/audits/audits-table.jsx
@@ -5,11 +5,27 @@ import API from '../../api/api';
 class AuditTable extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { audits: [], };
+    this.state = {
+      audits: [],
+      pagination: {
+        currentPage: 1,
+        perPage: 10,
+      },
+    };
     this.fetchAudits = this.fetchAudits.bind(this);
     if (process.env.NODE_ENV === 'test') return;
     // Continue initialization for non-test environments
     this.fetchAudits();
+  }
+
+  handleChangePage(newPage) {
+    const { pagination } = this.state;
+    this.setState({
+      pagination: {
+        ...pagination,
+        currentPage: newPage,
+      }
+    });
   }
 
   fetchAudits() {
@@ -22,10 +38,49 @@ class AuditTable extends React.Component {
       });
   }
 
-  render() {
-    const { audits } = this.state;
+  renderPagination() {
+    const { audits, pagination } = this.state;
 
-    const listItems = audits.map((item) => (
+    const pages = Array.from({ length: audits.length / pagination.perPage }, (_, i) => i + 1);
+
+    return (
+      <div>
+        {pages.map((page) => {
+          const pageStyle = page === pagination.currentPage
+            ? {
+              background: '#abc'
+            }
+            : {
+              background: '#999'
+            };
+
+          return (
+            <button
+              type="button"
+              style={{
+                padding: '0.33rem',
+                margin: '0.25rem',
+                ...pageStyle,
+              }}
+              onClick={() => this.handleChangePage(page)}
+            >
+              {page}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
+
+  render() {
+    const { audits, pagination } = this.state;
+
+    const visibleAudits = audits.slice(
+      (pagination.currentPage - 1) * pagination.perPage,
+      pagination.currentPage * pagination.perPage,
+    );
+
+    const listItems = visibleAudits.map((item) => (
       <tr key={item.id}>
         <td>{item.id}</td>
         <td>{item.event}</td>
@@ -46,6 +101,9 @@ class AuditTable extends React.Component {
               </span>
             </div>
           </div>
+
+          {this.renderPagination()}
+
           <table className="table table-hover table-responsive-sm">
             <thead>
               <tr>
