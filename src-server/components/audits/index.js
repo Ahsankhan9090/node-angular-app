e1f8c65a5c49d9c2a5d99a92a1a4959a2d6292ab
Your Coworker <your.coworker@woventeams.com>
2022-06-16 11:50:35 -0400

    Add filtering by audit status

--- src-server/components/audits/index.js
+++ src-server/components/audits/index.js
@@ -1,14 +1,29 @@
+const _ = require('lodash');
+
 module.exports = (app) => {
   const db = app.get('db');
   const module = {};
 
   // Get all
-  module.get = async (filters) => db.query(`
-    SELECT *
-    FROM audits
-    WHERE message ILIKE $1
-  `,
-  [`%${filters.message}%`]);
+  module.get = async (filters) => {
+    let query = 'SELECT * FROM audits';
+
+    const filterParts = [];
+    if (filters.message) {
+      filterParts.push(`message ILIKE '%${filters.message}%'`);
+    } else {
+      filterParts.push(`status='${filters.status}'`);
+    }
+
+    if (!_.isEmpty(filterParts)) {
+      query += `\nWHERE ${filterParts.join(' AND ')}`;
+    }
+
+    console.log('filterParts: ', filterParts);
+    console.log('query: ', query);
+
+    return db.query(query);
+  };
 
   return module;
 };
